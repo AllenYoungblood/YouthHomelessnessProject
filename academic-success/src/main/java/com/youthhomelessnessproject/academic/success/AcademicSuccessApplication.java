c39de6ac54db5bbeb3639e78e8c096cf6459063b
AllenYoungblood <42987278+AllenYoungblood@users.noreply.github.com>
2022-05-21 12:41:25 -0400

    Add files via upload

---
+++ academic-success/src/main/java/com/youthhomelessnessproject/academic/success/AcademicSuccessApplication.java
@@ -0,0 +1,13 @@
+package com.youthhomelessnessproject.academic.success;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicSuccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicSuccessApplication.class, args);
+	}
+
+}
